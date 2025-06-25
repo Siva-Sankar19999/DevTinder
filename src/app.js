ca8025963e8eb2c7d91931ed808fcc0aae1edf56
smuralasetty <smuralasetty@webmd.net>
2025-06-25 19:06:21 +0530

    Database Init

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const connectDB = require("./config/database");
-
+const User = require("./models/user")
 
 const app = express();
+
+
+
+app.post('/signup',async(req,res,next)=>{
+    const user1 = new User({firstName: "Ramesh1",lastName: "Pandi",email: "two@gmail.com",password:"ramesh@123"});
+
+    try{
+        await user1.save();
+        res.send("Data Posted successfullly");
+    }
+    catch(err){
+        next(err);
+    }    
+});
+
+app.use((err,req,res,next)=>{
+    if(err){
+        res.status(401).send("GOT issue saving in database..!");
+    }
+});
+
 
 
 connectDB()
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://sivasankar:6Z0Ukb8PosskQMgw@namastey-node.u6zm5vi.mongodb.net/User");
+    await mongoose.connect("mongodb+srv://sivasankar:6Z0Ukb8PosskQMgw@namastey-node.u6zm5vi.mongodb.net/devTinder");
 }
 
 module.exports = connectDB;
