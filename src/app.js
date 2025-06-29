946fed6c64c76aeced2f794cd55df7f1583e8ecd
smuralasetty <smuralasetty@webmd.net>
2025-06-29 16:08:11 +0530

    Wrote Different API's in Node

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const connectDB = require("./config/database");
-const User = require("./models/user")
+const User = require("./models/user");
 
 const app = express();
+
+app.use(express.json());
 
 
 
 app.post('/signup',async(req,res,next)=>{
-    const user1 = new User({firstName: "Ramesh1",lastName: "Pandi",email: "two@gmail.com",password:"ramesh@123"});
+
+    
+    console.log(req.body);
+     const user1 = new User(req.body);
 
     try{
         await user1.save();
@@ -15,15 +20,96 @@ app.post('/signup',async(req,res,next)=>{
     }
     catch(err){
         next(err);
-    }    
+    }
+
+    
 });
 
-app.use((err,req,res,next)=>{
-    if(err){
-        res.status(401).send("GOT issue saving in database..!");
+// get user
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.password;
+    try{
+        const user = await User.find({password:userEmail});
+        console.log(user);
+        if(user.length == 0){
+            res.status(404).send("User not found");
+        }
+        else {
+            res.send(user);
+        }
+            
+    
+    }
+    catch(err){
+        res.send("Error occured while fetching Database..!");
     }
 });
 
+
+//Feed API to get all the users
+app.get("/feed",async(req,res)=>{
+    try{
+        console.log("coming to this route..!");
+        
+        const users = await User.find({});
+        if(users.length == 0){
+            res.status(404).send("No users found for this route");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(403).send("Error occured connecting database..!");
+    }
+    
+});
+
+// Delete API
+app.delete('/deleteUser',async(req,res)=>{
+    const user = req.body.id;
+    try{
+        const deletedValue = await User.findByIdAndDelete(user);
+        res.send(deletedValue);
+
+    }
+    catch(err){
+        res.status(404).send('Error occured in deleting data..!');
+    }
+});
+
+//update API
+app.patch("/updateUser",async (req,res)=>{
+    const id = req.body.userId;
+    const data = req.body;
+    try{
+        let beforeData = await User.findByIdAndUpdate(id,data);
+        console.log(beforeData);
+        res.send("Data updated successfully..!");
+
+    }
+    catch(err){
+        res.send("Error occured in updating user..!");
+    }
+    
+});
+
+
+app.use((req,res)=>{
+    res.status(404).send("No Route there for that request..!");
+});
+
+
+
+app.use((err,req,res,next)=>{
+    console.log("executing..!");
+    if(err){
+        res.status(401).send("GOT issue ..!");
+    }
+    else{
+        res.status(404).send("No route there bhayaa...!");
+    }
+});
 
 
 connectDB()
