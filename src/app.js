62be4b6a544175a127f6d8ceabae79152b6f13c7
smuralasetty <smuralasetty@webmd.net>
2025-06-29 22:03:07 +0530

    Added API level validation

--- src/app.js
+++ src/app.js
@@ -78,17 +78,26 @@ app.delete('/deleteUser',async(req,res)=>{
 });
 
 //update API
-app.patch("/updateUser",async (req,res)=>{
-    const id = req.body.userId;
+app.patch("/updateUser/:userId",async (req,res)=>{
+    const id = req.params?.userId;
+    console.log(id,"checking ID value..!");
     const data = req.body;
+    
+    
     try{
+
+        const ALLOWED_FIELDS = ["lastName","firstName","age","gender"];
+        const isAllowed = Object.keys(req.body).every((input)=>{return ALLOWED_FIELDS.includes(input)});
+        if(!isAllowed){
+            throw new Error("Update not allowed..!");
+        }
         let beforeData = await User.findByIdAndUpdate(id,data);
         console.log(beforeData);
         res.send("Data updated successfully..!");
 
     }
     catch(err){
-        res.send("Error occured in updating user..!");
+        res.status(404).send("Error occured in updating user..!"+err);
     }
     
 });
